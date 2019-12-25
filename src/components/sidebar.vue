9c74284d17f55056e8c284ce47c09a41fc133e2b
gabrielhephzibah <gabrielhephzibah24@gmail>
2019-12-25 23:55:48 +0100

    pushed vuesax project to git

---
+++ src/components/sidebar.vue
@@ -0,0 +1,120 @@
+<template>
+<div class="main">
+	<div class="row">
+		<div class="col-2">
+		<div class="side">
+			<nav class="side-nav">
+			<div class="logo pt-2">
+				<img src="logo.png" ><span>Hand's maid international</span>
+				</div>
+
+				<div class="dash pt-2">
+				<a href<img src="dash.png"><span>Dashboard</span>
+				</div>
+
+				<div class="dash pt-2">
+				<a href<img src="dash.png"><span>Student</span>
+				</div>
+
+				<div class="dash pt-2">
+				<a href<img src="dash.png"><span>emoloyee</span>
+				</div>
+
+				<div class="dash pt-2">
+				<a href<img src="dash.png"><span>guardian</span>
+				</div>
+
+				<div class="dash pt-2">
+				<a href<img src="dash.png"><span>attendance</span>
+				</div>
+
+				<div class="dash pt-2">
+				<a href<img src="dash.png"><span>finance</span>
+				</div>
+				<div class="dash pt-2">
+				<a href<img src="dash.png"><span>subject</span>
+				</div>
+
+				<div class="dash pt-2">
+				<a href<img src="dash.png"><span>batches</span>
+				</div>
+
+				<div class="dash pt-2">
+				<a href<img src="dash.png"><span>report cards</span>
+				</div>
+
+				<div class="dash pt-2">
+				<a href<img src="dash.png"><span>SMS</span>
+				</div>
+
+				<div class="dash pt-2">
+				<a href<img src="dash.png"><span>Events</span>
+				</div>
+
+    		</nav>
+  			</div>
+		</div>
+	</div>
+</div>
+</template>
+
+
+<style>
+* {
+	margin:0;
+	padding:0;
+}
+.main{
+	background-color:#f9f9f9;
+}
+.side{
+
+ 	background:#00ADEE;
+    border: 1px solid ##00ADEE;
+    box-shadow: 0px 0px 0px ;
+    border-radius: 0px;
+    width: 75%;
+    height: 670px;
+}
+
+.side-nav{
+	background: #00ADEE;
+    border: 1px solid ##00ADEE;
+    box-shadow: 0px 2px 10px ;
+    border-radius: 0px;
+    width: 100%;
+    height: 670px;
+}
+.logo{
+	height :37px;
+	color : #33333;
+	background-color: #ffff;
+	width: 100%;
+	font-size: 9px;
+
+	 
+}
+.logo span{
+	padding-left:8px;
+}
+
+.dash{
+	height :37px;
+	color : #ffff;
+	background-color: #00ADEE;
+	width: 100%;
+	font-size: 10px;
+
+	 
+}
+.dash span{
+	padding-left:8px;
+}
+
+
+
+
+
+</style>
+
+
